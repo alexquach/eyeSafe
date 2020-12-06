daa41c690fc601f02a559490fd2931a755119047
Qasim Wani <qasim31wani@gmail.com>
2020-12-06 08:16:58 +0300

    storage demo wip

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -6,7 +6,38 @@ function poll_data() {
     xhr.addEventListener('readystatechange', function () {
         if (xhr.readyState == 4) {
         if (xhr.status == 200) {
-            console.log(JSON.parse(xhr.response));
+
+            const count = JSON.parse(xhr.response)["count"];
+            const times = JSON.parse(xhr.response)["datetime_array"];
+            
+            console.log("COUNTER", JSON.parse(xhr.response), count);
+
+            chrome.storage.sync.get("eyeSafe", data=>{
+                let eyeSafe = data.eyeSafe;
+                if (eyeSafe) {
+                    eyeSafe["days"]["get_date"]["count"] += count;
+                    eyeSafe["days"]["get_date"]["times"].push(times);
+
+                    chrome.storage.sync.set({"eyeSafe" : eyeSafe}, post_data=>{
+                        console.log("Appended data!");
+                        console.log(eyeSafe);
+                    });
+                }
+                else {
+                    /* Create new object */
+                    eyeSafe = {
+                        "days": {}
+                    };
+                    eyeSafe["days"]["get_date"] = {
+                        "count" : count,
+                        "times" : times
+                    };
+                    chrome.storage.sync.set({"eyeSafe" : eyeSafe}, post_data=>{
+                        console.log("Saved data!");
+                        console.log(eyeSafe);
+                    });
+                }
+            });
         }
       }
     });
@@ -15,4 +46,4 @@ function poll_data() {
 }
 
 
-setInterval(poll_data, 10000);
+setInterval(poll_data, 20000);
